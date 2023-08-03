5b04764ef1dbae6565b3635e6fe616e9be488b02
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-03 16:53:20 -0600

    modifique la funtion cliente para que sea asincrona

--- service/client-service.js
+++ service/client-service.js
@@ -1,7 +1,7 @@
 // funcion para crear la tablaa
-const crearNuevaLista = (nombre,email) => {
+const crearNuevaLista = (nombre, email) => {
   const linea = document.createElement("tr")
-  const contenido =`
+  const contenido = `
   <td class="td" data-td>${nombre}</td>
   <td>${email}</td>
   <td>
@@ -24,22 +24,42 @@ const crearNuevaLista = (nombre,email) => {
 // recorre todo el arbol del DOM(HTML) y obtiene data-table
 const table = document.querySelector("[data-table]")
 
-const http = new XMLHttpRequest(); //comunicacion entre el front y el backend
+// promesa, funcion asincrona
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => { //indicamosque va a ser asincrona
+    const http = new XMLHttpRequest(); //comunicacion entre el front y el backend
 
-// abre el metodo get
-http.open("GET", "http://localhost:3000/perfil")
+    // abre el metodo get
+    http.open("GET", "http://localhost:3000/perfil")
 
-// envia la petecion
-http.send()
+    // envia la petecion
+    http.send()
 
-// funcion para obtener datos del servidor
-http.onload = () => {
-  //la funcio JSON Lo convierte en un objeto JS
-    const data =  JSON.parse(http.response); //Aquí, se toma la respuesta del servidor 
-    console.log(data);
+    // funcion para obtener datos del servidor
+    http.onload = () => {
+      //la funcio JSON Lo convierte en un objeto JS
+      const response = JSON.parse(http.response); //Aquí, se toma la respuesta del servidor 
+      if (http.status >= 400) {
+        reject(response)
+      } else {
+        resolve(response)
+      }
+    };
+  });
+  return promise;
+}
+
+// Llamar a la función listaClientes para obtener los datos y trabajar con ellos
+listaClientes()
+  // La función then() se ejecutará cuando la promesa se resuelva correctamente
+  .then((data) => {
     // se raliza un bucle atravex de cada elemeto dentro del array 'data'
     data.forEach(perfil => { //y cada objeto representa representa un perfil de usuario
       const nuevaLinea = crearNuevaLista(perfil.nombre, perfil.email);
       table.appendChild(nuevaLinea); //se agrega la nueva fila a la tabla
     });
-};
+  })
+  .catch((error) => alert("Ocurrio un error"));
+
+
+
