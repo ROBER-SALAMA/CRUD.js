6b95d8cb5963b4d797071801e21f04857558cd7a
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-03 15:27:52 -0600

    agregue la funcion para crear tabla y para obtener datos del seeervidor

---
+++ service/client-service.js
@@ -0,0 +1,45 @@
+// funcion para crear la tablaa
+const crearNuevaLista = (nombre,email) => {
+  const linea = document.createElement("tr")
+  const contenido =`
+  <td class="td" data-td>${nombre}</td>
+  <td>${email}</td>
+  <td>
+    <ul class="table__button-control">
+      <li>
+        <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
+      </li>
+      <li>
+        <button class="simple-button simple-button--delete" type="button">
+          Eliminar
+        </button>
+      </li>
+    </ul>
+  </td>`
+  // De esta manera, la fila se rellena con el contenido generado 
+  linea.innerHTML = contenido
+  return linea // devuelve el nodo tr correctamnte
+}
+
+// recorre todo el arbol del DOM(HTML) y obtiene data-table
+const table = document.querySelector("[data-table]")
+
+const http = new XMLHttpRequest(); //comunicacion entre el front y el backend
+
+// abre el metodo get
+http.open("GET", "http://localhost:3000/perfil")
+
+// envia la petecion
+http.send()
+
+// funcion para obtener datos del servidor
+http.onload = () => {
+  //la funcio JSON Lo convierte en un objeto JS
+    const data =  JSON.parse(http.response); //Aquí, se toma la respuesta del servidor 
+    console.log(data);
+    // se raliza un bucle atravex de cada elemeto dentro del array 'data'
+    data.forEach(perfil => { //y cada objeto representa representa un perfil de usuario
+      const nuevaLinea = crearNuevaLista(perfil.nombre, perfil.email);
+      table.appendChild(nuevaLinea); //se agrega la nueva fila a la tabla
+    });
+};
