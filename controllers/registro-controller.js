77d86d1b459b87dd2f64ed05936632d207ad794b
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-05 16:37:26 -0600

    agregue service/client-service.js con la funcio que recive los datos del form y los guara en la bd

---
+++ controllers/registro-controller.js
@@ -0,0 +1,21 @@
+import { clienteServices } from "../service/client-service.js";
+
+//! recorre el elemento del DON en busca del data
+const formulario = document.querySelector("[data-form]");
+
+
+//! funcion que recive los datos del formulario
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault(); //?quita el comportamiento por defecto
+    //? .value obtiene el valor requerido
+    const nombre = document.querySelector("[data-nombre]").value
+    const email = document.querySelector("[data-email]").value
+    console.log(nombre, "-" , email);
+    //? accion para crear cliente
+    clienteServices
+    .crearCliente(nombre, email)
+    .then(respuesta => {
+        window.location.href = "/screens/registro_completado.html"
+    })
+    .catch(error => console.log(error))
+});
