78f3c4f362803c908e6a0695748628f29c8ce406
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-07 12:56:29 -0600

    agregue la funcio que obtien el id para posteriormente elimiarlo

--- controllers/client-controller.js
+++ controllers/client-controller.js
@@ -5,8 +5,8 @@
 
 import { clienteServices } from "../service/client-service.js"
 
-// funcion para crear la tablaa
-const crearNuevaLista = (nombre, email) => {
+//! funcion para crear la tablaa
+const crearNuevaLista = (nombre, email, id) => {
     const linea = document.createElement("tr")
     const contenido = `
     <td class="td" data-td>${nombre}</td>
@@ -17,29 +17,45 @@ const crearNuevaLista = (nombre, email) => {
           <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
         </li>
         <li>
-          <button class="simple-button simple-button--delete" type="button">
+          <button class="simple-button simple-button--delete" type="button"  id="${id}">
             Eliminar
           </button>
         </li>
       </ul>
     </td>`
-    // De esta manera, la fila se rellena con el contenido generado 
+    //? De esta manera, la fila se rellena con el contenido generado 
     linea.innerHTML = contenido
-    return linea // devuelve el nodo tr correctamnte
+
+    //! obtenemos el id para eliminarlo
+    const btn = linea.querySelector("button") 
+    btn.addEventListener('click', () =>{
+      const id = btn.id
+    //? accedemos a la funcion que va a eliminar
+      clienteServices
+        .eliminarCliente(id)
+        .then((respuesta) =>{
+          console.log(respuesta);
+        })
+        .catch((error) => alert("Sucedio un error"));
+    });
+    
+    return linea //? devuelve el nodo tr correctamnte
 }
 
 
-// recorre todo el arbol del DOM(HTML) y obtiene data-table
+//? recorre todo el arbol del DOM(HTML) y obtiene data-table
 const table = document.querySelector("[data-table]")
 
-// Llamar a la función listaClientes para obtener los datos y trabajar con ellos
+
+//! Llamar a la función listaClientes para obtener los datos y trabajar con ellos
 clienteServices.listaClientes()
-    // La función then() se ejecutará cuando la promesa se resuelva correctamente
+    //? La función then() se ejecutará cuando la promesa se resuelva correctamente
     .then((data) => {
-        // se raliza un bucle atravex de cada elemeto dentro del array 'data'
-        data.forEach(perfil => { //y cada objeto representa representa un perfil de usuario
-            const nuevaLinea = crearNuevaLista(perfil.nombre, perfil.email);
-            table.appendChild(nuevaLinea); //se agrega la nueva fila a la tabla
+        data.forEach(({nombre, email, id}) => { 
+            const nuevaLinea = crearNuevaLista(nombre, email, id);
+            table.appendChild(nuevaLinea); //? se agrega la nueva fila a la tabla
         });
     })
     .catch((error) => alert("Ocurrio un error"));
+
+    
